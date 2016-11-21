5eb2d4b438e319560c2b3807d791a0d908174a89
withered-flowers <wendy.hardianto@gmail.com>
2016-11-21 17:30:23 +0700

    update: 1st day

--- app/src/main/java/com/example/android/ud853/finalproject/app/MainActivity.java
+++ app/src/main/java/com/example/android/ud853/finalproject/app/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.android.ud853.finalproject.app;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class MainActivity extends ActionBarActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState == null) {
+            //TODO add fragment for main menu here !
+            /*
+            getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, new ForecastFragment())
+                .commit();
+            */
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
     }
 }
