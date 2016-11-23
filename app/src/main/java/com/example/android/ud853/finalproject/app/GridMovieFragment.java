8ee034f8d747f50e836f8356cde306ee57267d60
withered-flowers <wendy.hardianto@gmail.com>
2016-11-23 09:42:38 +0700

    Fetch data done, settings almost complete (summary remains)

--- app/src/main/java/com/example/android/ud853/finalproject/app/GridMovieFragment.java
+++ app/src/main/java/com/example/android/ud853/finalproject/app/GridMovieFragment.java
@@ -1,7 +1,9 @@
 package com.example.android.ud853.finalproject.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,13 +23,6 @@ public class GridMovieFragment extends Fragment {
 
     GridView grdMovieList;
 
-    static final String[] numbers = new String[] {
-            "A", "B", "C", "D", "E",
-            "F", "G", "H", "I", "J",
-            "K", "L", "M", "N", "O",
-            "P", "Q", "R", "S", "T",
-            "U", "V", "W", "X", "Y", "Z"};
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,40 +31,54 @@ public class GridMovieFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.fragment_gridmovie, container, false);
-
         grdMovieList = (GridView) rootView.findViewById(R.id.grdMovieList);
+        return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateList();
+    }
+
+    private void updateList() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String sortBy = prefs.getString(
+            getString(R.string.pref_movie_sortby_key),
+            getString(R.string.pref_movie_sortby_option_default_value)
+        );
 
         MovieDataFetcher theFetcher = new MovieDataFetcher();
         MovieInterfaces theInterface = theFetcher.getFetcher().create(MovieInterfaces.class);
         Call<MovieData> callMovieData = theInterface
-            .getMoviesBySort("popular", BuildConfig.MOVIE_DB_API_KEY_V3);
+            .getMoviesBySort(sortBy, BuildConfig.MOVIE_DB_API_KEY_V3);
         
         callMovieData.enqueue(new Callback<MovieData>() {
             @Override
             public void onResponse(Call<MovieData> call, Response<MovieData> resp) {
-                if(resp != null) {
-                    List<MovieObject> listMovies = resp.body().getDataResults();
+                assert resp != null;
+                List<MovieObject> listMovies = resp.body().getDataResults();
 
-                    //TODO Allocate the movie here !
-                    String[] arrString = new String[listMovies.size()];
-                    int index = 0;
-                    for(MovieObject obj : listMovies) {
-                        arrString[index] = String.valueOf(obj.getMovieOriginalTitle());
-                        index++;
-                    }
-
-                    ArrayAdapter<String> adp = new ArrayAdapter<String>(
-                        getActivity(),
-                        android.R.layout.simple_list_item_1,
-                        arrString
-                    );
-                    grdMovieList.setAdapter(adp);
-                    //ENDOFTODO
-
-                    Log.d(LOG_TAG, "Total Film: " + listMovies.size());
+                assert listMovies.size() != 0;
+                //TODO Allocate the movie here !
+                String[] arrString = new String[listMovies.size()];
+                int index = 0;
+                for(MovieObject obj : listMovies) {
+                    arrString[index] = String.valueOf(obj.getMovieOriginalTitle());
+                    index++;
                 }
+
+                ArrayAdapter<String> adp = new ArrayAdapter<String>(
+                    getActivity(),
+                    android.R.layout.simple_list_item_1,
+                    arrString
+                );
+                grdMovieList.setAdapter(adp);
+                //ENDOFTODO
+
+                Log.d(LOG_TAG, "Total Film: " + listMovies.size());
+                
             }
 
             @Override
@@ -77,8 +86,5 @@ public class GridMovieFragment extends Fragment {
                 Log.e(LOG_TAG, t.toString());
             }
         });
-
-        return rootView;
     }
-
 }
