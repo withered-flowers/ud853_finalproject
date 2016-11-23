411c3bc6d87ed6e5a2056431fb8484b77ae7e5dd
withered-flowers <wendy.hardianto@gmail.com>
2016-11-23 22:35:42 +0700

    Implementing Picasso

--- app/src/main/java/com/example/android/ud853/finalproject/adapter/MovieObjectAdapter.java
+++ app/src/main/java/com/example/android/ud853/finalproject/adapter/MovieObjectAdapter.java
@@ -6,11 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
 import java.util.List;
+import com.squareup.picasso.Picasso;
 
 import com.example.android.ud853.finalproject.app.R;
 import com.example.android.ud853.finalproject.backend.MovieObject;
+
 
 public class MovieObjectAdapter extends ArrayAdapter<MovieObject> {
     public MovieObjectAdapter(Activity context, List<MovieObject> movieObjectList) {
@@ -26,8 +27,10 @@ public class MovieObjectAdapter extends ArrayAdapter<MovieObject> {
                 .inflate(R.layout.fragment_gridmovie_eachmovie, parent, false);
         }
 
-        TextView movieImage = (TextView) convertView.findViewById(R.id.grdMovieList_image);
-        movieImage.setText(obj.getMovieOriginalTitle());
+        ImageView movieImage = (ImageView) convertView.findViewById(R.id.grdMovieList_image);
+
+        String imageUrl = "http://image.tmdb.org/t/p/w185/";
+        Picasso.with(getContext()).load(imageUrl + obj.getMoviePosterPath()).into(movieImage);
         //TODO Fetch Image here !
         //movieImage.setImageResource();
 
