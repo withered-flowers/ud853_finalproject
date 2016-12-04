5c4424132ffd6cb4af4e06b69ce12abcaea1d5ec
withered-flowers <wendy.hardianto@gmail.com>
2016-12-04 17:23:02 +0700

    Optimize import, remove implementation onSharedPreferencesChangedListener

--- app/src/main/java/com/example/android/ud853/finalproject/app/SettingsFragment.java
+++ app/src/main/java/com/example/android/ud853/finalproject/app/SettingsFragment.java
@@ -1,13 +1,9 @@
 package com.example.android.ud853.finalproject.app;
 
 import android.os.Bundle;
-import android.content.SharedPreferences;
-import android.preference.Preference;
-import android.preference.ListPreference;
 import android.preference.PreferenceFragment;
 
-public class SettingsFragment extends PreferenceFragment
-    implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsFragment extends PreferenceFragment {
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,13 +11,4 @@ public class SettingsFragment extends PreferenceFragment
         addPreferencesFromResource(R.xml.settings);
     }
 
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        Preference pref = findPreference(key);
-        sharedPreferences.getString(key, "");
-
-        if (pref instanceof ListPreference) {
-            ListPreference listPref = (ListPreference) pref;       
-        }
-    }
 }
